e654785d4d7bb58dba431d7a3a7e973bfef1b3f1
Matthew-PargaManasse <LithoBreakerGB@gmail.com>
2025-03-17 10:33:13 -0700

    Changed gpu settings. Added modesetting and switched from prime offload to sync

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -61,10 +61,11 @@
   #Graphics
   hardware.graphics.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.modsetting.enable = true;
   hardware.nvidia.open = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.production;
   hardware.nvidia.prime = {
-   offload.enable = true ;
+   sync.enable = true ;
    nvidiaBusId = "PCI:1:0:0";
    amdgpuBusId = "PCI:5:0:0";
   };
