9309b62eb8081b00a599716d62deb88bf91613e0
Matthew-PargaManasse <LithoBreakerGB@gmail.com>
2026-06-10 14:35:13 -0700

    fix(home): move utilities and mimeApps into base.nix so they actually install

--- home/base.nix
+++ home/base.nix
@@ -23,10 +23,34 @@
     obsidian
     ripgrep
     wget
+    
+    # Basic Desktop Utilities
+    gnome-text-editor
+    gnome-calculator
+    evince
+    file-roller
   ];
 
   home.sessionVariables = {
     EDITOR = "nvim";
+  };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "image/jpeg" = [ "imv.desktop" ];
+      "image/png" = [ "imv.desktop" ];
+      "image/gif" = [ "imv.desktop" ];
+      "image/webp" = [ "imv.desktop" ];
+      "image/svg+xml" = [ "imv.desktop" ];
+      "video/mp4" = [ "mpv.desktop" ];
+      "video/x-matroska" = [ "mpv.desktop" ];
+      "video/webm" = [ "mpv.desktop" ];
+      "video/quicktime" = [ "mpv.desktop" ];
+      "text/plain" = [ "org.gnome.TextEditor.desktop" ];
+      "text/markdown" = [ "org.gnome.TextEditor.desktop" ];
+      "application/pdf" = [ "org.gnome.Evince.desktop" ];
+    };
   };
 
   programs.home-manager.enable = true;
